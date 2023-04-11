49deb5f9e093405a8685c6497043749b36633d96
Dissmind <dissmind@gmail.com>
2023-04-11 20:37:44 +0200

    jwt token generation

---
+++ client/src/pages/registrationPage.jsx
@@ -0,0 +1,25 @@
+
+
+export const LoginPage = () => {
+
+
+
+
+  return (
+    <>
+      <div>
+        <div>Login: </div>
+        <input type="text" />
+      </div>
+
+      <div>
+        <div>Password: </div>
+        <input type="text" />
+      </div>
+
+      <div>
+        <button>Sign In</button>
+      </div>
+    </>
+  )
+}
